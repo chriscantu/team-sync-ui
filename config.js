899a3eb6bf9fe99639ef48a34a421a343d0af13b
Chris Cantu <christopher.canut@gmail.com>
2015-03-09 00:19:23 -0500

    Updated dependencies

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ System.config({
     "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.9.3",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.2",
     "aurelia-framework": "github:aurelia/framework@0.8.6",
-    "aurelia-http-client": "github:aurelia/http-client@0.4.4",
+    "aurelia-http-client": "github:aurelia/http-client@0.5.3",
     "aurelia-router": "github:aurelia/router@0.5.4",
     "font-awesome": "npm:font-awesome@4.3.0",
     "foundation": "github:zurb/bower-foundation@5.5.1",
@@ -54,14 +54,14 @@ System.config({
       "aurelia-history": "github:aurelia/history@0.2.2",
       "core-js": "npm:core-js@0.4.10"
     },
-    "github:aurelia/http-client@0.4.4": {
-      "aurelia-path": "github:aurelia/path@0.4.2",
+    "github:aurelia/http-client@0.5.3": {
+      "aurelia-path": "github:aurelia/path@0.4.3",
       "core-js": "npm:core-js@0.4.10"
     },
     "github:aurelia/loader-default@0.4.1": {
       "aurelia-loader": "github:aurelia/loader@0.3.3",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.2"
+      "aurelia-path": "github:aurelia/path@0.4.3"
     },
     "github:aurelia/loader@0.3.3": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
@@ -71,7 +71,7 @@ System.config({
     "github:aurelia/router@0.5.4": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.2",
       "aurelia-history": "github:aurelia/history@0.2.2",
-      "aurelia-path": "github:aurelia/path@0.4.2",
+      "aurelia-path": "github:aurelia/path@0.4.3",
       "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.2.2",
       "core-js": "npm:core-js@0.4.10"
     },
@@ -88,7 +88,7 @@ System.config({
     "github:aurelia/templating-router@0.9.2": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.2",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.2",
+      "aurelia-path": "github:aurelia/path@0.4.3",
       "aurelia-router": "github:aurelia/router@0.5.4",
       "aurelia-templating": "github:aurelia/templating@0.8.8"
     },
@@ -99,7 +99,7 @@ System.config({
       "aurelia-loader": "github:aurelia/loader@0.3.3",
       "aurelia-logging": "github:aurelia/logging@0.2.2",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.2",
+      "aurelia-path": "github:aurelia/path@0.4.3",
       "aurelia-task-queue": "github:aurelia/task-queue@0.2.3",
       "core-js": "npm:core-js@0.4.10"
     },
